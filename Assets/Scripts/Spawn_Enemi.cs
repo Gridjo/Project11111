133a380628cfb220268454c6739e2979d119cc91
Gridjo <94613659+Gridjo@users.noreply.github.com>
2022-06-02 17:34:07 +0300

    dg

---
+++ Assets/Scripts/Spawn_Enemi.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Enemi : MonoBehaviour
+{
+    public GameObject enemy;
+    public Transform enemyPos;
+    public float repeatRate = 5.0f
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
