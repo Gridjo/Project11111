2c26139a1e362e09697152684811170229e16f59
Gridjo <94613659+Gridjo@users.noreply.github.com>
2022-06-08 14:05:38 +0300

    newversion 228

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,35 +6,36 @@ using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
-    public float timeToSpawn = 2f, timeToSpawnClone = 2f;
+    public float timeToSpawn = 10f, timeToSpawnClone = 10f;
     public GameObject[] spawnPoint;
     public GameObject enemyPoolMeles;
     public GameObject enemyPoolRanges;
     public wave[] waves;
     private int WaveNomber = 0;
 
-   
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-         
-         
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(timeToSpawn <= 0 && waves.Length >= WaveNomber)
+
+        if (timeToSpawn <= 0 && waves.Length >= WaveNomber)
         {
 
             for (int i = 0; waves[WaveNomber].meles >= i; i++)
             {
                 var curEnemy = enemyPoolMeles.transform.GetChild(0);
                 curEnemy.SetParent(spawnPoint[UnityEngine.Random.Range(0, spawnPoint.Length)].transform, false);
+                gameObject.transform.localPosition = new Vector3();
                 curEnemy.gameObject.SetActive(true);
 
             }
@@ -42,16 +44,17 @@ public class SpawnEnemy : MonoBehaviour
             {
                 var curEnemy = enemyPoolRanges.transform.GetChild(0);
                 curEnemy.SetParent(spawnPoint[UnityEngine.Random.Range(0, spawnPoint.Length)].transform, false);
+                gameObject.transform.localPosition = new Vector3();
                 curEnemy.gameObject.SetActive(true);
             }
             timeToSpawn = timeToSpawnClone;
-            k++;
+            WaveNomber++;
         }
         timeToSpawn -= Time.deltaTime;
     }
 
     [Serializable]
-    public struct wave  
+    public struct wave
     {
         public int meles;
         public int ranges;
