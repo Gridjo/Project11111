9426db7abd4d2a609e3172060a9183d2359f66a4
Gridjo <94613659+Gridjo@users.noreply.github.com>
2022-06-17 19:30:02 +0300

    fff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,6 +17,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Energy < MaxEnergy)
         {
             if (TimeEner <= 0)
