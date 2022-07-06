71b3d4b64ad758146a3b0de08a0d0f6c29e08bb9
Gridjo <94613659+Gridjo@users.noreply.github.com>
2022-07-06 14:07:15 +0300

    ehyyyyyy

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -1,7 +1,7 @@
 
 using System;
-using UnityEngine;
 using System.Collections;
+using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
@@ -11,15 +11,18 @@ public class SpawnEnemy : MonoBehaviour
     public GameObject enemyPoolRanges;
     public GameObject enemyPoolNpc;
     public GameObject GameManager;
-    public wave[] waves;/*
-    public seconds[] second;*/
+    public wave[] waves;
     public int WaveNomber = 0;
     public bool canSpawnM = true, canSpawnR = true, canSpawnN = true;
+    public bool canSpawnMA = true, canSpawnRA = true, canSpawnNA = true;
+    public bool canSpawnAM = false, canSpawnAR = false, canSpawnAN = false;
     public bool endOfWaveAndCanSpawnModuls = false;
     public GameObject BarCenter;
     public GameObject BarLeft;
     public GameObject BarRight;
-    /*public float timeSp = 0f;*/
+    private int iM = 1;
+    private int iR = 1;
+    private int iN = 1;
 
 
 
@@ -32,9 +35,15 @@ public class SpawnEnemy : MonoBehaviour
 
     // Update is called once per frame
 
-    public void SpawnM(GameObject[] spawnPoint, GameObject enemyPoolMeles)
+    IEnumerator SpawnM(GameObject[] spawnPoint, GameObject enemyPoolMeles)
     {
-        
+
+
+
+        canSpawnM = false;
+        canSpawnR = false;
+        canSpawnN = false;
+        yield return new WaitForSeconds(1f);
         var curEnemy = enemyPoolMeles.transform.GetChild(0);
         curEnemy.SetParent(spawnPoint[UnityEngine.Random.Range(0, spawnPoint.Length)].transform, false);
         gameObject.transform.localPosition = new Vector3();
@@ -43,77 +52,85 @@ public class SpawnEnemy : MonoBehaviour
         curEnemy.gameObject.GetComponent<Enemy>().BarRight = BarRight;
         curEnemy.gameObject.GetComponent<Enemy>().BarLeft = BarLeft;
         curEnemy.gameObject.SetActive(true);
+
+        canSpawnM = true;
+        canSpawnR = true;
+        canSpawnN = true;
+        Sppp();
+
     }
-    
+    public void Sppp()
+    {
+        if (waves.Length - 1 >= WaveNomber && canSpawnM && canSpawnN && canSpawnR)
+        {
+            for (int i = 1; waves[WaveNomber].Npc >= iN && canSpawnN /*&& second[(int)timeSp].Npc >= i*/; iN++)
+            {
+                StartCoroutine(SpawnM(spawnPoint, enemyPoolNpc));
+
+                if (waves[WaveNomber].Npc <= iN)
+                {
+                    canSpawnNA = false;
+                    canSpawnAN = true;
+                }
+
+            }
+            for (int i = 0; waves[WaveNomber].meles >= iM && canSpawnM; iM++)
+            {
+                StartCoroutine(SpawnM(spawnPoint, enemyPoolMeles));
+
+                if (waves[WaveNomber].meles <= iM)
+                {
+                    canSpawnMA = false;
+                    canSpawnAM = true;
+                }
+            }
+
+            for (int i = 1; waves[WaveNomber].ranges >= iR && canSpawnR /*&& second[(int)timeSp].ranges >= i*/; iR++)
+            {
+                StartCoroutine(SpawnM(spawnPoint, enemyPoolRanges));
+
+                if (waves[WaveNomber].ranges <= iR)
+                {
+                    canSpawnRA = false;
+                    canSpawnAR = true;
+                }
+            }
+
+            
+
+            if (canSpawnAM && canSpawnAN && canSpawnAR)
+            {
+                timeToSpawn = timeToSpawnClone;
+                WaveNomber++;
+                canSpawnM = false;
+                canSpawnR = false;
+                canSpawnN = false;
+                iN = 1;
+                iR = 1;
+                iM = 1;
+            }
+            if (!canSpawnMA && !canSpawnNA && !canSpawnRA)
+            {
+
+            }
+            SPVD();
+        }
+
+    }
+
     void Update()
     {
 
-        if (timeToSpawn <= 0 && waves.Length - 1 >= WaveNomber/* && timeSp <= 10f*/)
+        if (timeToSpawn <= 0)
         {
-            /*if (timeSp >= 1.1f)
-                timeSp = 0;*/
-            if (true)
-            {
-                for (int i = 1; waves[WaveNomber].meles >= i && canSpawnM /*&& second[(int)timeSp].meles >= i*/; )
-                {
-                    /*if (timeSp >=1f) {*/
-                        SpawnM(spawnPoint, enemyPoolMeles);
-                        i++;
-                        if (waves[WaveNomber].meles <= i)
-                            canSpawnM = false;
-                      /*  if (timeSp >= 1.1f)
-                            timeSp = 0;
-                    }*/
-                }
+            Sppp();
 
-                for (int i = 1; waves[WaveNomber].ranges >= i && canSpawnR /*&& second[(int)timeSp].ranges >= i*/; )
-                {
-                    /*if (timeSp >= 1f)
-                    {*/
-                        SpawnM(spawnPoint, enemyPoolRanges);
-                        i++;
-                        if (waves[WaveNomber].ranges <= i)
-                            canSpawnR = false;
-                      /*  if (timeSp >= 1.1f)
-                            timeSp = 0;
-                    }*/
-                }
 
-                for (int i = 1; waves[WaveNomber].Npc >= i && canSpawnN /*&& second[(int)timeSp].Npc >= i*/; )
-                {
-                        /*if (timeSp >= 1f)
-                        {
-                           */ SpawnM(spawnPoint, enemyPoolNpc);
-                            i++;
-                            if (waves[WaveNomber].Npc <= i)
-                                canSpawnN = false;
-                      /*  if (timeSp >= 1.1f)
-                            timeSp = 0;
-                    }*/
-                } 
-            }
-            
-            timeToSpawn = timeToSpawnClone;
-            SPVD();
-            /*if (timeSp >= 1.1f)
-                timeSp = 0;
-*/
-
-            WaveNomber++;
-            canSpawnM = true;
-            canSpawnR = true;
-            canSpawnN = true;
-            endOfWaveAndCanSpawnModuls = true;
         }
         timeToSpawn -= Time.deltaTime;
-        /*timeSp += Time.deltaTime;*/
-        /*timeSp += Time.deltaTime;*/
-        /*timeSp += Time.deltaTime;
-        if (timeSp >= 11f)
-            timeSp = 0;*/
     }
     void SPVD()
-    { 
+    {
         try
         {
             GameManager.GetComponent<GameManager>().ChSpawnModul();
@@ -132,13 +149,4 @@ public class SpawnEnemy : MonoBehaviour
         public int ranges;
         public int Npc;
     }
-   /* [Serializable]
-    public struct seconds
-    {
-        public float time;
-        
-        public int meles;
-        public int ranges;
-        public int Npc;
-    }*/
 }
